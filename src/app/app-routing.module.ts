dc9ffc1c17ec0e849ffaee00a7650f74b93881ac
Vitly7 <111552374+Vitly7@users.noreply.github.com>
2023-12-01 10:53:43 +0700

    d

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Dashboard2Component } from './dashboard2/dashboard2.component';
 import { Dashboard3Component } from './dashboard3/dashboard3.component';
 import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+import { otentikasiGuard } from './otentikasi.guard';
 
 const routes: Routes = [
   { path: 'admin', component: AdminComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'dashboard2', component: Dashboard2Component },
+  { path: 'dashboard', component: DashboardComponent, canActivate : [otentikasiGuard] },
+  { path: 'dashboard2', component: Dashboard2Component, canActivate : [otentikasiGuard] },
   { path: 'dashboard3', component: Dashboard3Component },
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'mahasiswa', component: MahasiswaComponent },
+  { path: 'mahasiswa', component: MahasiswaComponent, canActivate : [otentikasiGuard] },
 ];
 
 @NgModule({
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,49 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
+declare const $: any;
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  constructor(private router: Router, private http: HttpClient) {}
   @Input() moduleName: string = '';
+
+  showPeringatanModal(message: string): void {
+    $('#peringatanModal').modal();
+    $('#pm_message').html(message);
+  }
+
+  logOut(): void {
+    console.log('logOut');
+
+    var userId = $('#idText').val();
+    userId = encodeURIComponent(userId);
+
+    var password = $('#passwordText').val();
+    password = encodeURIComponent(password);
+
+    var url =
+      'https://stmikpontianak.net/011100862/login.php' +
+      '?id=' +
+      userId +
+      '&password=' +
+      password;
+
+    console.log('url :  ' + url);
+
+    this.http.get(url).subscribe((data: any) => {
+      console.log(data);
+      var row = data[0];
+
+      sessionStorage.removeItem('userId', userId);
+
+      console.log('session data berhasil dihapus');
+
+      this.router.navigate(['/dashboard']);
+    });
+  }
 }
